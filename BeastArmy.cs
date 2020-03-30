fe23ba6ca154c609fd609a9d04441d65fa7d3984
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 14:00:55 +0200

    Initial commit

---
+++ BeastArmy.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace RolePlayV21
+{
+    public class BeastArmy
+    {
+        private List<Beast> _army;
+
+        public BeastArmy()
+        {
+            _army = new List<Beast>();
+        }
+
+        /// <summary>
+        /// Add one Beast to the army 
+        /// </summary>
+        public void AddBeast(Beast aBeast)
+        {
+            _army.Add(aBeast);
+        }
+
+        /// <summary>
+        /// Dead is defined as: All members of the army must be dead
+        /// </summary>
+        public bool Dead
+        {
+            get
+            {
+                return false; // TODO - implement as specified
+            } 
+        }
+
+        /// <summary>
+        /// Returns the names of all Beasts that are currently alive
+        /// </summary>
+        public List<string> BeastsAlive
+        {
+            get
+            {
+                List<string> alive = new List<string>();
+
+                // TODO - implement as specified
+
+                foreach (Beast b in _army)
+                {
+                    if (!b.Dead)
+                    {
+                        alive.Add(b.Name);
+                    }
+                }
+                return alive;
+            }
+        }
+
+        /// <summary>
+        /// DealDamage is defined as: 
+        /// The total damage dealt by all
+        /// non-dead members of the army
+        /// </summary>
+        public int DealDamage()
+        {
+            int totalDamage = 0;
+
+            foreach (Beast b in _army)
+            {
+                if (!b.Dead)
+                {
+                    totalDamage = totalDamage + b.DealDamage();
+                }
+            }
+
+            // TODO - implement as specified
+
+            return totalDamage;
+        }
+
+        /// <summary>
+        /// ReceiveDamage is defined as: 
+        /// The first non-dead beast in the list 
+        /// receives all of the damage
+        /// </summary>
+        public void ReceiveDamage(int damage)
+        {
+            
+            // TODO - implement as specified
+        }
+    }
+}
